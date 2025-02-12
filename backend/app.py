762fa27a1f6f32374e329b9ce87a25f88e3884e5
Nitil Poddar <npoddar.zjef@gmail.com>
2025-02-12 07:25:17 +0530

    pushing

--- backend/app.py
+++ backend/app.py
@@ -1,62 +1,38 @@
-from flask import Flask, render_template, url_for, redirect, request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from flask import Flask, request, url_for, redirect, render_template
+from flask_restful import Api, Resource
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy.orm import declarative_base
+from import_files.models import db, User, Item, Bill, Bill_Items
 
-class Base(DeclarativeBase):
-    pass
 
 app = Flask(__name__)
 
-db = SQLAlchemy(model_class=Base)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 
 db.init_app(app)
 
-class Item(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, unique=True)
-    name: Mapped[str] = mapped_column(unique=True)
-    price: Mapped[int] = mapped_column(default=0)
-    stock: Mapped[int] = mapped_column(default=0)
-
-
 @app.route('/')
-def Index():
-    return render_template("index.html")
+def index():
+    return render_template('index.html')
 
-@app.route('/add-item', methods=['GET', 'POST'])
-def AddProduct():
+@app.route('/addItem', methods=['GET','POST'])
+def add_item():
     if request.method == 'GET':
-        itemdata = db.session.execute(db.select(Item).order_by(Item.id)).scalars()
-        return render_template("add-item.html", items = itemdata, css_file = 'add-item.css' )
+        return render_template('add-item.html')
     else:
-        # return redirect(url_for("Index"))
         name = request.form.get("name")
         price = request.form.get("price")
         stock = request.form.get("stock")
-
-        newItem = Item(name = name, price = price, stock = stock)
-        db.session.add(newItem)
+        description = request.form.get("description")
+        item = Item(name=name, price=price, stock=stock, description=description)
+        db.session.add(item)
         db.session.commit()
-        return redirect(url_for("AddProduct"))
-        # return f'the name is {name} and the price is {price} and the stock is {stock} !!!!!!'
+        return f"name is {name}, price is {price}, stock is {stock}, description is {description}"
+        
 
-
-@app.route('/inventory')
-def Inventory():
-    return render_template("index.html")
-
-@app.route('/customer')
-def Customer():
-    return render_template("index.html")
-
-@app.route('/billing')
-def Billing():
-    return render_template("index.html")
 
 if __name__ == '__main__':
+    # app.run(debug=True)
     with app.app_context():
         db.create_all()
-        app.run(
-            debug=True
-        )
-
+    app.run(debug=True)
